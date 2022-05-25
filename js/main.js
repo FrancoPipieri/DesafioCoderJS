54b4d13778f9757a47473b9aef0618402f6cb2ab
Franco Pipieri <pipierif98@gmail.com>
2022-05-25 17:52:12 -0300

    Desafio DOM

---
+++ js/main.js
@@ -0,0 +1,183 @@
+const agregarArticuloACarrito = document.querySelectorAll('.agregarACarrito');
+agregarArticuloACarrito.forEach((agregarACarrito) => {
+  agregarACarrito.addEventListener('click', agregarACarritoClicked);
+});
+
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
+
+const contenedorDelCarrito = document.querySelector(
+  '.contenedorDelCarrito'
+);
+
+function agregarACarritoClicked(event) {
+  const button = event.target;
+  const item = button.closest('.item');
+
+  const itemTitle = item.querySelector('.item-title').textContent;
+  const itemPrice = item.querySelector('.item-price').textContent;
+  const itemImage = item.querySelector('.item-image').src;
+
+  agregarItemAlCarrito(itemTitle, itemPrice, itemImage);
+}
+
+function agregarItemAlCarrito(itemTitle, itemPrice, itemImage) {
+  const tituloElementos = contenedorDelCarrito.getElementsByClassName(
+    'tituloDeItem'
+  );
+  for (let i = 0; i < tituloElementos.length; i++) {
+    if (tituloElementos[i].innerText === itemTitle) {
+      let elementQuantity = tituloElementos[i].parentElement.parentElement.parentElement.querySelector('.cantidadItemsCarrito');
+      elementQuantity.value++;
+
+      $('.toast').toast('show');
+      actualizCarritoCompraTotal();
+      return;
+    }
+  }
+  let contendioDelCarrito = 0;
+  const renglonCarrito = document.createElement('div');
+
+    if(itemTitle === "Marker Uniposca"){
+      contendioDelCarrito = `<div class="row itemCarritoCompras">
+        <div class="col-3">
+          <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom">
+            <img src=${itemImage} class="imagenEnCarrito">
+            <h6 class="shopping-cart-item-title tituloDeItem text-truncate ml-3 mb-0">${itemTitle}</h6>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="shopping-cart-price d-flex justify-content-center align-items-center h-100 border-bottom">
+            <p class="item-price mb-0 precioItemCarrito">${itemPrice}</p>
+          </div>
+        </div>
+        <div class="col-2 align-self-center">
+        <input class="inputColor" type="text">
+        </div>
+        <div class="col-5">
+          <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom">
+              <input class="shopping-cart-quantity-input cantidadItemsCarrito col-4 ms-3" type="number" value="1">
+
+            <select class="col-4 ms-3 align-self-center">
+              <option value="1.7mm">1.7mm</option>
+              <option value="1.3">1.3mm</option>
+              <option value="2.5">2.5mm</option>
+              <option value="8">8mm</option>
+              <option value="15">15mm</option>
+            </select>
+              <button class="btnBorrar btn btn-danger buttonDelete col-2" type="button">X</button>
+          </div>
+        </div>
+      </div>`;}
+    else if(itemTitle === "Squeezer Grog"){
+      contendioDelCarrito = `<div class="row itemCarritoCompras">
+      <div class="col-3">
+          <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom">
+              <img src=${itemImage} class="imagenEnCarrito">
+              <h6 class="shopping-cart-item-title tituloDeItem text-truncate ml-3 mb-0">${itemTitle}</h6>
+          </div>
+      </div>
+      <div class="col-2">
+          <div class="shopping-cart-price d-flex justify-content-center align-items-center h-100 border-bottom">
+              <p class="item-price mb-0 precioItemCarrito">${itemPrice}</p>
+          </div>
+      </div>
+      <div class="col-2 align-self-center">
+      <input class="inputColor" type="text">
+      </div>
+      <div class="col-5 ">
+          <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom">
+              <input class="shopping-cart-quantity-input cantidadItemsCarrito col-4 ms-3" type="number" value="1">
+
+              <select class="col-4 ms-3 align-self-center">
+              <option value="5mm">5mm</option>
+              <option value="10">10mm</option>
+              <option value="20">20mm</option>
+              <option value="25">25mm</option>
+              </select>
+              <button class="btnBorrar btn btn-danger buttonDelete col-2" type="button">X</button>
+          </div>
+      </div>
+      </div>`;  
+    }else if(itemTitle === "Crayon Markal"){
+      contendioDelCarrito = `<div class="row itemCarritoCompras">
+      <div class="col-3">
+          <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom">
+              <img src=${itemImage} class="imagenEnCarrito">
+              <h6 class="shopping-cart-item-title tituloDeItem text-truncate ml-3 mb-0">${itemTitle}</h6>
+          </div>
+      </div>
+      <div class="col-2">
+          <div class="shopping-cart-price d-flex justify-content-center align-items-center h-100 border-bottom">
+              <p class="item-price mb-0 precioItemCarrito">${itemPrice}</p>
+          </div>
+      </div>
+      <div class="col-2 align-self-center">
+      <input class="inputColor" type="text">
+      </div>
+      <div class="col-5">
+          <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom">
+              <input class="shopping-cart-quantity-input cantidadItemsCarrito col-4 ms-3" type="number" value="1">
+
+              <select class="col-4 ms-3">
+              <option value="17mm">17mm</option>
+              </select>
+              <button class="btnBorrar btn btn-danger buttonDelete col-2" type="button">X</button>
+          </div>
+      </div>
+    </div>`;
+    }
+  renglonCarrito.innerHTML = contendioDelCarrito;
+  contenedorDelCarrito.append(renglonCarrito);
+
+  renglonCarrito
+    .querySelector('.buttonDelete')
+    .addEventListener('click', removeShoppingCartItem);
+
+  renglonCarrito
+    .querySelector('.cantidadItemsCarrito')
+    .addEventListener('change', cambioCantidad);
+
+  actuazilarCarritoCompral();
+}
+
+function actuazilarCarritoCompra() {
+  let total = 0;
+  const totalCarritoCompra = document.querySelector('.totalCarritoCompra');
+
+  const ItemsdeCarrito = document.querySelectorAll('.itemCarritoCompras');
+
+  ItemsdeCarrito.forEach((itemCarritoCompras) => {
+    const preciodelElementoCarrito = itemCarritoCompras.querySelector(
+      '.precioItemCarrito'
+    );
+    const precioItemCarrito = Number(
+      preciodelElementoCarrito.textContent.replace('$', '')
+    );
+    const cantidadElementos = itemCarritoCompras.querySelector(
+      '.cantidadItemsCarrito'
+    );
+    const cantidadItemsCarrito = Number(
+      cantidadElementos.value
+    );
+    total = total + precioItemCarrito * cantidadItemsCarrito;
+  });
+  totalCarritoCompra.innerHTML = `   ${total.toFixed(2)} $`;
+}
+
+function removeShoppingCartItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.closest('.itemCarritoCompras').remove();
+  actuazilarCarritoCompra();
+}
+
+function cambioCantidad(event) {
+  const input = event.target;
+  input.value <= 0 ? (input.value = 1) : null;
+  actuazilarCarritoCompra();
+}
+
+function comprarButtonClicked() {
+  contenedorDelCarrito.innerHTML = '';
+  actuazilarCarritoCompra();
+}
